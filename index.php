d1af0ae2cac8f1cf7eb72df8b25f0cae5e3a2e23
Naresh Khasu <groupdjcompany@gmail.com>
2020-01-05 12:00:22 +0545

    inital commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <header class="header">
+        <a class="logo">online portal</a>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label for="menu-btn" class="menu-icon">
+            <span class="nav-icon"></span>
+        </label>
+        <ul class="menu">
+            <li><a>home</a></li>
+            <li><a>politics</a></li>
+            <li><a>business</a></li>
+            <li><a>interview</a></li>
+            <li><a>lifestyle</a></li>
+            <li><a>travel</a></li>
+        </ul>
+    </header>
+</body>
+
+</html>
