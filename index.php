cde2f071ce65ba019c77f2b5937d62ac02a3912f
Naresh Khasu <groupdjcompany@gmail.com>
2020-01-07 14:24:20 +0545

    CSs update

--- index.php
+++ index.php
@@ -10,21 +10,30 @@
 </head>
 
 <body>
-    <header class="header">
-        <a class="logo">online portal</a>
-        <input class="menu-btn" type="checkbox" id="menu-btn" />
-        <label for="menu-btn" class="menu-icon">
+    <div class="head-wrapper">
+        <div class="advertisement">
+            <img src="https://www.genesiswtech.com/wp-content/uploads/2020/01/NMB-Monk_Nari-1140x90_1.gif">
+        </div>
+        <div class="site-main-title">
+            <img src="https://img.icons8.com/color/48/000000/news.png">
+        </div>
+        <header class="header">
+            <a class="logo">online portal</a>
+            <input class="menu-btn" type="checkbox" id="menu-btn" />
+            <label for="menu-btn" class="menu-icon">
             <span class="nav-icon"></span>
         </label>
-        <ul class="menu">
-            <li><a>home</a></li>
-            <li><a>politics</a></li>
-            <li><a>business</a></li>
-            <li><a>interview</a></li>
-            <li><a>lifestyle</a></li>
-            <li><a>travel</a></li>
-        </ul>
-    </header>
+            <ul class="menu">
+                <li><a>home</a></li>
+                <li><a>politics</a></li>
+                <li><a>business</a></li>
+                <li><a>interview</a></li>
+                <li><a>lifestyle</a></li>
+                <li><a>travel</a></li>
+            </ul>
+        </header>
+    </div>
+    <?php include('footer.php'); ?>
 </body>
 
 </html>
